dcb829bb0be8941e70a7f544e5e037fbc50ca1ac
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2023-01-24 18:36:50 +0800

    Add story for Begin event

--- scripts/events.js
+++ scripts/events.js
@@ -1,7 +1,7 @@
 export const events = {
     begin: {
         text: function (game) {
-            return `text here`;
+            return `You wake up on a hard bed. The room is cold and dark, and you feed a persistent, throbbing pain in your head. You don't remember where you are, nor how you got here. As you sit up, you feel a hard, rounded object in your pocket. A pendant. It is made out of a strange, translucent green stone that emits a faint glow. Holding the pendant in your palm, you seem to feel a constant trickle of warmth flowing from the pendant. Or maybe it is just your imagination.`;
         },
         actions: [
             {
--- scripts/events.ts
+++ scripts/events.ts
@@ -3,7 +3,7 @@ import { gameStruct } from "./settings.js";
 export const events = {
     begin: {
         text: function (game: gameStruct): string {
-            return `text here`;
+            return `You wake up on a hard bed. The room is cold and dark, and you feed a persistent, throbbing pain in your head. You don't remember where you are, nor how you got here. As you sit up, you feel a hard, rounded object in your pocket. A pendant. It is made out of a strange, translucent green stone that emits a faint glow. Holding the pendant in your palm, you seem to feel a constant trickle of warmth flowing into your hand. Or maybe it is just your imagination.`;
         },
         actions: [
             {
