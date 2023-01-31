69a8a6fc572674501f24aa22714f8ada0f23cba2
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2023-01-31 19:15:03 +0800

    Add story for Explore Village 2

--- src/events/explorevillage.js
+++ src/events/explorevillage.js
@@ -3,7 +3,7 @@ export default {
         text: function (game) {
             return `You walk slowly to the heart of the village. 
             As you go on, houses become bigger and more intact.
-            A large monolithic spire of stone materialises from the deep fog, a monument to the forgotten past.`;
+            A large monolithic spire of stone materialises from the deep fog, a monument of the forgotten past.`;
         },
         actions: [
             {
@@ -24,6 +24,52 @@ export default {
             return `You walk away from the center of the village.
             Survived structures become scarcer and smaller, until the sandy floor gives away to an endless plain of grass.
             Out here, only a few structures stand, scattered far along the parched landscape.`;
+        },
+        actions: [
+            {
+                name: function (game) {
+                    return "continue";
+                },
+                action: function (game) {
+                    return {
+                        game,
+                        nextEvent: "explorevillage_2_edges"
+                    };
+                }
+            }
+        ]
+    },
+    explorevillage_2_center: {
+        text: function (game) {
+            return `Standing in front of the spire, it towers over you, casting a long, narrow shadow over everything.
+            The elements have evidently taken its toll, as long cracks run down the structure after years of wind and rain.
+            You spot a small dusty glass door, half covered by pebbles, but still functional.
+            You gently push open the door, and the rusted hinges creak, after being opened for the first time in a long while.
+            Dust falls from the ceiling as if it was going to collapse, but you gently walk in.`;
+        },
+        actions: [
+            {
+                name: function (game) {
+                    return "continue";
+                },
+                action: function (game) {
+                    return {
+                        game,
+                        nextEvent: "explorevillage_2_edges"
+                    };
+                }
+            }
+        ]
+    },
+    explorevillage_2_edges: {
+        text: function (game) {
+            return `You are far from the village.
+            The mist makes it hard to see far, but you spot what looks like an abandoned house in the distance.
+            You slowly walk closer.
+            You find a small old-fashioned two-story wooden house.
+            It seems to be empty, probably abandoned during whatever apocalypse happened to this place.
+            A dusty wooden door, paint peeling, hangs invitingly half-open.
+            The door creaks as you push open it and step in.`;
         },
         actions: [
             {
